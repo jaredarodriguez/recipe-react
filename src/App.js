da82664900cd8ec89c9650e203ceed428c718b33
Jared Rodriguez <jaredarodriguez@gmail.com>
2019-10-15 12:47:46 -0500

    api fetching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
@@ -7,13 +6,25 @@ const App = () => {
   const APP_ID = 'd172fffa';
   const API_KEY = "0dc8ee686a919c496640ef5ede142d9b";
 
-  const exampleReq =
-    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`
+  const [recipes, setRecipes] = useState([]);
+
+
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`)
+    const data = await response.json();
+    console.log(data.hits);
+  }
+
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button className="search-button" type="submit">Search</button>
+        <button className="search-button" type="submit">Search </button>
       </form>
     </div>
   )
